ebd00952b7e8f0c3a2cc40c031f09ea4a35633d1
sungjoonkim <ted@bitflow.ai>
2025-03-06 02:05:39 +0900

    config adjustment

--- setup.cmd
+++ setup.cmd
@@ -1,8 +1,9 @@
 REM create an use a virtual env (venv) - optional
+REM python -m pip install pillow diffusers loguru
+python -m pip install torch==2.5.1+cu124 torchvision==0.20.1+cu124 torchaudio==2.5.1+cu124 --index-url https://download.pytorch.org/whl/cu124
 pip install virtualenv
-python -m pip install -r requirements.txt
 python -m venv .venv
 
 REM continue at Venv
 .\.venv\Scripts\activate
-python -m pip install -r requirements.txt
+pip install -r requirements.txt
